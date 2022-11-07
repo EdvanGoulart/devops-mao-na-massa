8ba0e5f928166aeb374b486345400bcd19ae626a
Edvan Goulart <edvangoulart76@gmail.com>
2022-11-07 09:29:11 -0300

    add files

---
+++ lab-jenkins/provision.sh
@@ -0,0 +1,38 @@
+#/bin/bash
+#c99c1297c18e45edb006d2b9a5abf280
+#TOKEN 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
+yum install epel-release -y
+yum install wget git -y
+sudo wget --no-check-certificate -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+yum install java-11-openjdk-devel -y
+yum install jenkins -y
+systemctl daemon-reload
+systemctl start jenkins
+systemctl enable jenkins
+
+#Instalação docker e docker-compose
+sudo yum install -y yum-utils
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+systemctl daemon-reload
+systemctl restart docker
+usermod -aG docker jenkins
+
+#intalação do sonar scanner
+yum install wget unzip -y
+wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.7.0.2747-linux.zip
+unzip sonar-scanner-cli-4.7.0.2747-linux.zip -d /opt/
+mv /opt/sonar-scanner-4.7.0.2747-linux /opt/sonar-scanner
+chown -R jenkins:jenkins /opt/sonar-scanner
+echo 'export PATH=$PATH:/opt/sonar-scanner/bin' | sudo tee -a /etc/profile
+curl -fsSL https://rpm.nodesource.com/setup_16.x | sudo bash -
+sudo yum install nodejs -y
